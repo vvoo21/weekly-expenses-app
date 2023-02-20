84b2c60792b86f5b11cec3af41232a6f36b18e9c
Vanessa O <vaneolipa@gmail.com>
2023-02-20 12:09:27 -0500

    Add the printAlert method

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -4,4 +4,22 @@ class UI {
       document.querySelector('#total').textContent = budget;
       document.querySelector('#remaining').textContent = remaining;
     }
+
+    static printAlert(message, type) {
+      const divMessage = document.createElement('div');
+      divMessage.classList.add('alert');
+  
+      if (type === 'error') {
+        divMessage.classList.add('error');
+      } else {
+        divMessage.classList.add('success');
+      }
+  
+      divMessage.textContent = message;
+      formSection.insertBefore(divMessage, formSection.children[1]);
+  
+      setTimeout(() => {
+        divMessage.remove();
+      }, 2000);
+    }
   }
