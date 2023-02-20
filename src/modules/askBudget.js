5bba21cda0c55b13558886343537a6678a6ba824
Vanessa O <vaneolipa@gmail.com>
2023-02-20 18:32:56 -0500

    add window.focus

--- src/modules/askBudget.js
+++ src/modules/askBudget.js
@@ -4,6 +4,7 @@ import UI from './UI.js';
 let budget;
 
 const askBudget = () => {
+  window.focus();
   const userBudget = Number(prompt('What is your budget?'));
 
   if (userBudget === '' || userBudget === null || Number.isNaN(userBudget) || userBudget <= 0) {
