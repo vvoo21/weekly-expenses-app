7a277832121847b123066695a3f6f0f038777bbe
Vanessa O <vaneolipa@gmail.com>
2023-02-20 12:13:58 -0500

    Create the formSection variable

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,25 +1,27 @@
+const formSection = document.querySelector('.form-section');
+
 class UI {
-    static addBudget(amount) {
-      const { budget, remaining } = amount;
-      document.querySelector('#total').textContent = budget;
-      document.querySelector('#remaining').textContent = remaining;
+  static addBudget(amount) {
+    const { budget, remaining } = amount;
+    document.querySelector('#total').textContent = budget;
+    document.querySelector('#remaining').textContent = remaining;
+  }
+
+  static printAlert(message, type) {
+    const divMessage = document.createElement('div');
+    divMessage.classList.add('alert');
+
+    if (type === 'error') {
+      divMessage.classList.add('error');
+    } else {
+      divMessage.classList.add('success');
     }
 
-    static printAlert(message, type) {
-      const divMessage = document.createElement('div');
-      divMessage.classList.add('alert');
-  
-      if (type === 'error') {
-        divMessage.classList.add('error');
-      } else {
-        divMessage.classList.add('success');
-      }
-  
-      divMessage.textContent = message;
-      formSection.insertBefore(divMessage, formSection.children[1]);
-  
-      setTimeout(() => {
-        divMessage.remove();
-      }, 2000);
-    }
+    divMessage.textContent = message;
+    formSection.insertBefore(divMessage, formSection.children[1]);
+
+    setTimeout(() => {
+      divMessage.remove();
+    }, 2000);
   }
+}
