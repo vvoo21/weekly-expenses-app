fb5464672f3fddc2b515dc0391d1015f8255fa7d
Vanessa O <vaneolipa@gmail.com>
2023-02-21 14:14:38 -0500

    Validate the ask budget  form and hide the modal when the response is correct

--- src/modules/askBudget.js
+++ src/modules/askBudget.js
@@ -3,16 +3,27 @@ import UI from './UI.js';
 
 let budget;
 
-const askBudget = () => {
-  const userBudget = prompt('What is your budget?');
+const askBudget = (e) => {
+  e.preventDefault();
+
+  const userBudget = document.querySelector('#ask-budget').value;
 
   if (userBudget === '' || userBudget === null || Number.isNaN(Number(userBudget)) || userBudget <= 0) {
-    window.location.reload();
+    const divMessage = document.createElement('div');
+    divMessage.classList.add('alert');
+    divMessage.textContent = 'Invalid amount';
+    document.querySelector('.ask-budget-section').appendChild(divMessage);
+    setTimeout(() => {
+      divMessage.remove();
+    }, 1000);
+    return
   }
 
   budget = new Budget(userBudget);
 
   UI.addBudget(budget);
+
+  document.querySelector('.ask-budget-section').style.display = 'none';
 };
 
 export default askBudget;
